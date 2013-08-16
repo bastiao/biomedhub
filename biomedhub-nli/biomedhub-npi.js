b643d617d8f7fc4f301caad64dbf8003fc6ee424
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-08-16 09:07:45 +0100

    Adding graphical part of Natural Language Interface

---
+++ biomedhub-nli/biomedhub-npi.js
@@ -0,0 +1,10 @@
+
+
+
+$('.example-countries .typeahead').typeahead({                                
+  name: 'countries',                                                          
+  prefetch: '../data/countries.json',                                         
+  limit: 10                                                                   
+});
+
+
